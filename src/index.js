5e1186aa3ce5db52dc5783418a86caf41a9fdfc8
Natalya Solovyova <ns-66@yandex.ru>
2022-02-20 22:33:36 +0700

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let openBrackets = bracketsConfig.map(inArr => inArr[0]);
+  let closeBrackets = bracketsConfig.map(inArr => inArr[1]);
+  let bracketPairs = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    bracketPairs[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElem = stack[stack.length - 1];
+
+    if (openBrackets.includes(currentSymbol)) {
+      if (stack.length > 0 && closeBrackets.includes(currentSymbol)) {
+        if (bracketPairs[currentSymbol] === topElem) {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (bracketPairs[currentSymbol] === topElem) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
+
